b3a43c4cdb8404ae4474b3ea813ff74a1c3ceebc
savage <146668608+Rinattttt@users.noreply.github.com>
2024-09-19 10:51:10 +0300

    Add files via upload

---
+++ process.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+try
+{
+    Process process = new Process();
+    process.StartInfo.FileName = "notepad.exe";
+    for (int i = 0; i < 2; i++)
+    {
+        Process.Start(process.StartInfo);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Произошла ошибка: " + ex.Message);
+}
